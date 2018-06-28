e65e0717c752dc1999d9540de895b6529cfbe05c
bslemma <bepoweru@gmail.com>
2018-06-28 14:05:33 -0700

    promise async and await

--- dnsToIP.js
+++ dnsToIP.js
@@ -12,4 +12,19 @@ dns.resolve4('www.mum.edu', function (err, addr) {
 // const dns = require('dns')
 // const res = promisify(dns.resolve4);
 // res('www.mum.edu').then((add)=>{console.log(add)}).catch(err=>console.log(err));
- 
+
+
+// const { promisify } = require('util');
+// const dns = require('dns');
+// const res = promisify(dns.resolve4);
+
+// async function main() {
+//     try{
+//        const data= await res('www.mum.edu');
+//         console.log(data);
+//     }
+//     catch(err){
+//         console.log('Error Message: ', err);
+//     }
+// }
+// main();
